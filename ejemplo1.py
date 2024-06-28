d83d7a713bfecaa1b25cc121a508cc9af5ed7a2b
Ema Cabral <micorreo2@gmail.com>
2024-06-28 20:48:49 -0300

    creo mi repo

---
+++ ejemplo1.py
@@ -0,0 +1,8 @@
+def mensaje1():
+    print("mi primera version")
+
+def mensaje3():
+    print("mi mensaje 3")
+
+def mensaje3():
+    print("mi mensaje 3")
---
+++ ejemplo2.py
@@ -0,0 +1,5 @@
+def mensaje2():
+    print("hola estoy trabajando con git")
+
+def mensaje4():
+    print("mi primera version 4")
